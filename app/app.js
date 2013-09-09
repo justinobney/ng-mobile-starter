ba334bafe68f0ba9da01764aa1f5e2deb61bd8fb
Justin Obney <justinobney@gmail.com>
2013-09-09 12:38:13 -0500

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,74 @@
+(function (angular) {
+
+	'use strict';
+
+	angular
+		.module('myApp', ['ek.mobileFrame'])
+
+		.config([
+			'$routeProvider',
+			'$mobileFrameProvider',
+			function ($routeProvider, $mobileFrameProvider) {
+
+				$routeProvider
+					.when('/', {
+						templateUrl: 'app/views/index.tmpl.html'
+					})
+					.when('/about/', {
+						templateUrl: 'app/views/about.tmpl.html'
+					})
+					.when('/search/:search', {
+						templateUrl: 'app/views/search-result.tmpl.html',
+						controller: 'SearchResultCtrl'
+					})
+					.otherwise({
+						redirectTo: '/'
+					});
+
+					$mobileFrameProvider
+						.setHeaderHeight(50)
+						.setFooterHeight(30)
+						.setNavWidth(200);
+
+			}
+		])
+
+		.controller('SearchFormCtrl', ['$scope', '$location', function ($scope, $location) {
+			$scope.onSearch = function () {
+				$location.url('/search/' + $scope.search);
+			};
+		}])
+
+		.controller('SearchResultCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+			$scope.term = $routeParams.search;
+
+			$scope.results = [{
+				title: 'Lorem ipsum ',
+				description: 'Short loin short ribs corned beef ball …'
+			}, {
+				title: 'Lorem ipsum',
+				description: 'Short loin short ribs corned beef ball …'
+			}, {
+				title: 'Lorem ipsum',
+				description: 'Short loin short ribs corned beef ball …'
+			}, {
+				title: 'Lorem ipsum',
+				description: 'Short loin short ribs corned beef ball …'
+			}];
+		}])
+
+		.directive('blurOnSubmit', function () {
+			return {
+				restrict: 'A',
+				link: function ($scope, $elem) {
+					$elem.bind('keyup', function (evnt) {
+						if ( evnt.keyCode !== 13 ) {
+							return;
+						}
+						this.blur();
+					});
+				}
+			};
+		});
+
+})(angular);
